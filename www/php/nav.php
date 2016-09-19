e796fa20296eec57c692f9c6b20729f4c28d025a
Isaac Sikkema <isaacsikkema@gmail.com>
2016-09-19 18:33:52 +0000

    Improved navbar

--- www/php/nav.php
+++ www/php/nav.php
@@ -10,12 +10,19 @@
     echo "
         <nav class='navbar navbar-default'>
             <div class='navbar-header'>
+                <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#navbar' onclick='setPostboxHeight(this);'>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                </button>
                 <a class='navbar-brand' href='#'>Council of One</a>
             </div>
-            <ul class='nav navbar-nav'>
-                <li ".$home."><a href='index.php'>Home</a></li>
-                <li ".$arch."><a href='archive.php'>Archive</a></li>
-                <li ".$abou."><a href='about.php'>About</a></li>
-            </ul>
+            <div class='collapse navbar-collapse' id='navbar'>
+                <ul class='nav navbar-nav'>
+                    <li ".$home."><a href='index.php'>Home</a></li>
+                    <li ".$arch."><a href='archive.php'>Archive</a></li>
+                    <li ".$abou."><a href='about.php'>About</a></li>
+                </ul>
+            </div>
         </nav>";
 ?>
